0e091f6f9694703c1ac1db708d0b20e38d6a5270
ThankGod Richard <thankgod@lemonade.finance>
2022-10-31 19:38:28 +0100

    ficed required changes

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 import ViewHandler from './modules/viewhandler.js';
+import { DateTime } from './modules/luxon.js';
 
 window.addEventListener(
   'load',
   () => {
     const viewhandler = new ViewHandler();
     viewhandler.initiew();
-    ViewHandler.setUpNav();
+
+    const dateAndTime = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+    const temppArr = dateAndTime.split(' ');
+    delete temppArr[temppArr.length - 1];
+    document.getElementById('navdate').innerHTML = temppArr.join(' ').replace(',', '');
+
+    document.getElementById('home').addEventListener('click', (e) => {
+      e.preventDefault();
+      document.getElementById('bokklist_div').style.display = 'block';
+      document.getElementById('addnew_div').style.display = 'none';
+      document.getElementById('contact_div').style.display = 'none';
+    });
+
+    document.getElementById('addnew').addEventListener('click', (e) => {
+      e.preventDefault();
+      document.getElementById('bokklist_div').style.display = 'none';
+      document.getElementById('addnew_div').style.display = 'block';
+      document.getElementById('contact_div').style.display = 'none';
+    });
+
+    document.getElementById('contact').addEventListener('click', (e) => {
+      e.preventDefault();
+      document.getElementById('bokklist_div').style.display = 'none';
+      document.getElementById('addnew_div').style.display = 'none';
+      document.getElementById('contact_div').style.display = 'block';
+    });
   },
   false,
 );
--- modules/viewhandler.js
+++ modules/viewhandler.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-restricted-globals */
 
-import { DateTime } from 'luxon';
 import Book from './book.js';
 import BookListManger from './boolist_manager.js';
 
@@ -31,8 +30,6 @@ export default class ViewHandler {
       liNode.appendChild(pNode);
       liNode.appendChild(buttonNode);
       this.bookListView.appendChild(liNode);
-      const dt = DateTime.now();
-      document.getElementById('nav_date').innerHTML = dt.toLocaleString(DateTime.DATETIME_MED);
     });
 
     document.getElementById('btn').addEventListener('click', (event) => {
@@ -42,29 +39,6 @@ export default class ViewHandler {
       const newBook = new Book(bookanme, author, new Date().getTime());
       this.bookListManager.addNewBook(newBook);
       location.reload();
-    });
-  }
-
-  static setUpNav() {
-    document.getElementById('home').addEventListener('click', (e) => {
-      e.preventDefault();
-      document.getElementById('bokklist_div').style.display = 'block';
-      document.getElementById('addnew_div').style.display = 'none';
-      document.getElementById('contact_div').style.display = 'none';
-    });
-
-    document.getElementById('addnew').addEventListener('click', (e) => {
-      e.preventDefault();
-      document.getElementById('bokklist_div').style.display = 'none';
-      document.getElementById('addnew_div').style.display = 'block';
-      document.getElementById('contact_div').style.display = 'none';
-    });
-
-    document.getElementById('contact').addEventListener('click', (e) => {
-      e.preventDefault();
-      document.getElementById('bokklist_div').style.display = 'none';
-      document.getElementById('addnew_div').style.display = 'none';
-      document.getElementById('contact_div').style.display = 'block';
     });
   }
 }
